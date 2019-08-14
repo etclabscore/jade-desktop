842c91ae199b3c3addfa4581a9c3740b4ade1598
shanejonas <jonas.shane@gmail.com>
2019-08-14 11:25:28 -0700

    feat: add ipc for stop command + download feature

--- electron/electron.ts
+++ electron/electron.ts
@@ -1,12 +1,18 @@
 import { menubar } from "menubar";
 import * as path from "path";
+import { ipcMain, BrowserWindow } from "electron";
 
-import { startServiceRunner, ConnectionInfo } from "@etclabscore/jade-service-runner";
+import { startServiceRunner, ConnectionInfo, ServiceRunnerServer } from "@etclabscore/jade-service-runner";
 
 const mb = menubar({
   index: process.env.NODE_ENV === "development"
     ? "http://localhost:3000/index.html"
     : `file://${path.join(__dirname, "../build/index.html")}`,
+  browserWindow: {
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  }
 });
 
 mb.on("ready", () => {
@@ -14,7 +20,20 @@ mb.on("ready", () => {
     host: "localhost",
     port: 8002,
     protocol: "http",
-  }]), "./services/").then(() => {
+  }]), "./services/").then((sr: ServiceRunnerServer) => {
+    ipcMain.on("service-runner-jsonrpc", async (event, arg) => {
+      switch (arg.method) {
+        case "stopService":
+          event.returnValue = await sr.serviceManager.stopService(
+            arg.params[0],
+            arg.params[1],
+            arg.params[2],
+          );
+          break;
+        default:
+          break;
+      }
+    });
     console.log("app is ready") //tslint:disable-line
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { AppBar, Typography, CssBaseline, Grid, Toolbar, IconButton, Card, CardContent, Chip, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core"; //tslint:disable-line
+import { AppBar, Typography, CssBaseline, Grid, Toolbar, IconButton, Card, CardContent, Chip, List, ListItem, ListItemIcon, ListItemText, CardHeader, Avatar, Button, CircularProgress } from "@material-ui/core"; //tslint:disable-line
 import ServiceRunnerClient from "@etclabscore/jade-service-runner-client";
 import useDarkMode from "use-dark-mode";
 import Brightness3Icon from "@material-ui/icons/Brightness3";
 import { ThemeProvider } from "@material-ui/styles";
 import WbSunnyIcon from "@material-ui/icons/WbSunny";
+import CloudDownload from "@material-ui/icons/CloudDownloadOutlined";
 import { darkTheme, lightTheme } from "./themes/jadeTheme";
+import useInterval from "@rooks/use-interval";
 import "./App.css";
+
+declare var window: Window;
+interface Window { //tslint:disable-line
+  require: any;
+}
+
+const { ipcRenderer } = window.require("electron");
 
 const App: React.FC = () => {
   const darkMode = useDarkMode();
@@ -15,6 +24,16 @@ const App: React.FC = () => {
   const [serviceRunnerClient, setServiceRunnerClient] = useState<ServiceRunnerClient>();
   const [runningServices, setRunningServices] = useState<any[]>([]);
   const [installedServices, setInstalledServices] = useState<any[]>([]);
+  const [downloadingServices, setDownloadingServices] = useState<any[]>([]);
+
+  const handleInstallService = async (name: string, version: string) => {
+    setDownloadingServices([...downloadingServices, name]);
+    if (serviceRunnerClient) {
+      await serviceRunnerClient.installService(name, version);
+      setDownloadingServices(downloadingServices);
+    }
+  };
+
   useEffect(() => {
     setServiceRunnerClient(new ServiceRunnerClient({
       transport: {
@@ -24,22 +43,19 @@ const App: React.FC = () => {
       },
     }));
   }, []);
-  useEffect(() => {
-    if (serviceRunnerClient) {
-      serviceRunnerClient.listServices("all").then(setServices);
-    }
-  }, [serviceRunnerClient]);
 
-  useEffect(() => {
+  const { start, stop } = useInterval(() => {
     if (serviceRunnerClient) {
-      serviceRunnerClient.listRunningServices().then(setRunningServices);
+      serviceRunnerClient.listServices("available").then(setServices);
+      serviceRunnerClient.listServices("running").then(setRunningServices);
+      serviceRunnerClient.listServices("installed").then(setInstalledServices);
     }
-  }, [services, serviceRunnerClient]);
-  useEffect(() => {
+  }, 1000, true);
+  function handleStart(name: string, version: string, env: string) {
     if (serviceRunnerClient) {
-      serviceRunnerClient.listInstalledServices().then(setInstalledServices);
+      serviceRunnerClient.startService(name, version, env);
     }
-  }, [services, serviceRunnerClient]);
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -72,54 +88,93 @@ const App: React.FC = () => {
             </Grid>
           </Toolbar>
         </AppBar>
-        <Grid container justify="space-around" style={{marginBottom: "-20px"}}>
-          <Card style={{background: "transparent"}}>
+        <Grid container justify="space-around" style={{ marginBottom: "-20px" }}>
+          <Card style={{ background: "transparent" }}>
             <CardContent>
               <Typography variant="h6">Available</Typography>
               <Typography variant="h3">{services ? services.length : 0}</Typography>
             </CardContent>
           </Card>
-          <Card style={{background: "transparent"}}>
+          <Card style={{ background: "transparent" }}>
             <CardContent>
               <Typography variant="h6">Installed</Typography>
               <Typography variant="h3">{installedServices ? installedServices.length : 0}</Typography>
             </CardContent>
           </Card>
-          <Card style={{background: "transparent"}}>
-            <CardContent>
-              <Typography variant="h6">Running</Typography>
-              <Typography variant="h3">{runningServices ? runningServices.length : 0}</Typography>
-            </CardContent>
-          </Card>
         </Grid>
         <div>
           {services.map((service) => {
+            const installedService = installedServices.find((s) => {
+              return s.name === service.name;
+            });
+            const downloadingService = downloadingServices.find((s: string) => s === service.name);
             return (
-              <Card style={{background: "transparent"}}>
+              <Card style={{ background: "transparent" }}>
                 <CardContent>
-                  <Typography variant="h6">{service.name}</Typography>
-                  <Chip label={service.version} />
-                  <Chip label={service.state} />
-                  {service.environments.length > 0 && <h2>Environments</h2>}
-                  <List component="nav" aria-label="main list services">
+                  <Card style={{ background: "transparent" }}>
+                    <CardHeader
+                      title={service.name}
+                      subheader={
+                        installedService
+                          ? "installed"
+                          : downloadingService
+                            ? "installing"
+                            : service.state
+                      }
+                      action={
+                        <Grid container direction="column" alignItems="flex-end">
+                          <Chip label={service.version} />
+                          {
+                            downloadingService ?
+                              <CircularProgress />
+                              :
+                              installedService ?
+                                null
+                                :
+                                <IconButton onClick={() => handleInstallService(service.name, service.version)}>
+                                  <CloudDownload />
+                                </IconButton>
+                          }
+                        </Grid>
+                      }
+                    >
+                    </CardHeader>
+                  </Card>
+                  {installedService && <List component="nav" aria-label="main list services">
                     {service.environments.map((e: any) => {
-                      const runningService = runningServices.find((s) => s.name === service.name && s.env === e);
+                      const runningService = runningServices.find((s) => {
+                        return s.name === service.name && s.environments.includes(e);
+                      });
                       return (
-                        <ListItem button>
+                        <ListItem>
                           <ListItemText primary={e} />
                           <ListItemIcon>
                             <>
+                              <Button disabled>
+                                {
+                                  runningService
+                                    ? "Running"
+                                    : "Not Running"
+                                }
+                              </Button>
                               {
                                 runningService
-                                  ? "Running"
-                                  : "Not Running"
+                                  ? <Button onClick={() => ipcRenderer.sendSync("service-runner-jsonrpc", {
+                                    method: "stopService",
+                                    params: [
+                                      service.name,
+                                      service.version,
+                                      e,
+                                    ],
+                                  })}>Stop</Button>
+                                  : <Button onClick={() => handleStart(service.name, service.version, e)}>Start</Button>
                               }
                             </>
                           </ListItemIcon>
                         </ListItem>
                       );
                     })}
-                  </List>
+                  </List>}
                 </CardContent>
               </Card>
             );
